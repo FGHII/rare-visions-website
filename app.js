8d11f2b6bf725156652bbab0f086533af81e20ee
FGHII <73318929+FGHII@users.noreply.github.com>
2021-09-21 00:07:36 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+//ExpressJS setup
+const express = require('express');
+const app = express();
+
+//EJS setup
+const ejsLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+// app.use(ejsLayouts);
+
+//Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Require static assets from public folder
+app.use(express.static('public'));
+
+
+app.get("/", function(req,res){
+    res.render("index");
+  });
+  
+app.get("/:pageName", function(req,res){
+    res.render(req.params.pageName);
+    });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+  console.log("Server started on port " + PORT);
+});
