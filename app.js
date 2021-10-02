6b811130bd6c3b1748c601c33e4bbd8206c6ca1e
FGHII <73318929+FGHII@users.noreply.github.com>
2021-10-02 10:37:55 -0500

    started nodemailer setup

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const app = express();
 
+//dotenv setup
+require('dotenv').config();
+
+//nodemailer setup
+const nodemailer = require('nodemailer');
+
 //EJS setup
 const ejsLayouts = require('express-ejs-layouts');
 const ejs = require('ejs');
@@ -15,11 +21,41 @@ app.use(express.urlencoded({ extended: true }));
 // Require static assets from public folder
 app.use(express.static('public'));
 
-
-app.get("/", (req,res) => {
+app.get('/', (req,res) => {
     res.render("index");
   });
-  
+
+
+//gmail not accepting messages from nodemailer, have to setup OAUTH2 authentication
+//https://www.youtube.com/watch?v=-rcRf7yswfM
+app.post('/', (req,res) => {
+  console.log(req.body);
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', 
+    auth: {
+      user: 'rarevisionslightshow@gmail.com',
+      pass: process.env.EMAIL_PASS
+    }
+  })
+  const mailOptions = {
+    from: req.body.email,
+    to: 'rarevisionslightshow@gmail.com',
+    subject: `Website message from ${req.body.email}`,
+    text: `Event date: ${req.body.date}
+    Message: ${req.body.message}`
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('success');
+    }
+  })
+
+})
 // app.get("/:pageName", function(req,res){
 //     res.render(req.params.pageName);
 //     });
---
+++ public/scripts/script.js
@@ -0,0 +1,58 @@
+//squish animation - eventually refactor get element by ID to queryselectorAll to reduce repetition
+const contactLink = document.getElementById('contact-link');
+const submitButton = document.getElementById('submit-button');
+const squishContact = document.getElementById('squish-contact')
+const squishSubmit = document.getElementById('squish-submit')
+
+
+contactLink.addEventListener('mouseenter', () => {
+    squishContact.classList.toggle('fade');
+  });
+
+contactLink.addEventListener('mouseleave', () => {
+    squishContact.classList.toggle('fade');
+});
+
+submitButton.addEventListener('mouseenter', () => {
+  squishSubmit.classList.toggle('fade');
+});
+
+submitButton.addEventListener('mouseleave', () => {
+  squishSubmit.classList.toggle('fade');
+});
+
+//contact form
+const contactForm = document.querySelector('.contact-form');
+let submitterName = document.getElementById('name');
+let email = document.getElementById('email');
+let date = document.getElementById('date');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let formData = {
+    name: submitterName.value,
+    email: email.value,
+    date: date.value,
+    message: message.value
+  }
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = () => {
+    console.log(xhr.responseText);
+    if(xhr.responseText == 'success') {
+      alert('Email sent');
+      submitterName.value = '';
+      email.value = '';
+      subject.value = '';
+      message.value = '';
+    } else {
+      alert('Error: email not sent');
+    }
+  }
+  xhr.send(JSON.stringify(formData));
+
+});
