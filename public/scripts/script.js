1aa359a7da1fddf1a9928fd9c85d8619e4952378
FGHII <73318929+FGHII@users.noreply.github.com>
2021-10-03 22:04:38 -0500

    disabled squish hover animation and mobile hover

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,25 +1,27 @@
+
+
 //squish animation - eventually refactor get element by ID to queryselectorAll to reduce repetition
-const contactLink = document.getElementById('contact-link');
-const submitButton = document.getElementById('submit-button');
-const squishContact = document.getElementById('squish-contact')
-const squishSubmit = document.getElementById('squish-submit')
+// const contactLink = document.getElementById('contact-link');
+// const submitButton = document.getElementById('submit-button');
+// const squishContact = document.getElementById('squish-contact')
+// const squishSubmit = document.getElementById('squish-submit')
 
 
-contactLink.addEventListener('mouseenter', () => {
-    squishContact.classList.toggle('fade');
-  });
+// contactLink.addEventListener('mouseenter', () => {
+//     squishContact.classList.toggle('fade');
+//   });
 
-contactLink.addEventListener('mouseleave', () => {
-    squishContact.classList.toggle('fade');
-});
+// contactLink.addEventListener('mouseleave', () => {
+//     squishContact.classList.toggle('fade');
+// });
 
-submitButton.addEventListener('mouseenter', () => {
-  squishSubmit.classList.toggle('fade');
-});
+// submitButton.addEventListener('mouseenter', () => {
+//   squishSubmit.classList.toggle('fade');
+// });
 
-submitButton.addEventListener('mouseleave', () => {
-  squishSubmit.classList.toggle('fade');
-});
+// submitButton.addEventListener('mouseleave', () => {
+//   squishSubmit.classList.toggle('fade');
+// });
 
 //contact form
 const contactForm = document.querySelector('.contact-form');
